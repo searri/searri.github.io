64ea5cc330d1f19c0ed9a7125cdcfc9b786c54f7
Rick Sear <yobob.sear@gmail.com>
2018-05-15 09:34:10 -0500

    A bit of debugging

--- scriptPackage.js
+++ scriptPackage.js
@@ -10,9 +10,10 @@ function changeColors() {
   document.getElementById("third").className = "alt-one-third column u-maxHeight";
   document.getElementById("twoThirds").className = "alt-two-thirds column u-maxHeight u-alt-background";
   var navigation = document.getElementsByClassName("Nav-link");
-  for (var i = 0; i < navigation.length; i++) {
-    navigation[i].className = "alt-Nav-link";
-  }
+  // for (var i = 0; i < navigation.length; i++) {
+  //   navigation[i].className = "alt-Nav-link";
+  // }
+  navigation[1].className = "alt-Nav-link";
 }
 
 function superScriptFiveMillion() {
