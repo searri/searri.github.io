09b561d6c96dff4fda541ae6815f63cb69890b81
Rick Sear <yobob.sear@gmail.com>
2018-05-15 09:39:58 -0500

    Maybe a while loop instead of a for loop?

--- scriptPackage.js
+++ scriptPackage.js
@@ -10,10 +10,13 @@ function changeColors() {
   document.getElementById("third").className = "alt-one-third column u-maxHeight";
   document.getElementById("twoThirds").className = "alt-two-thirds column u-maxHeight u-alt-background";
   var navigation = document.getElementsByClassName("Nav-link");
-  // for (var i = 0; i < navigation.length; i++) {
-  //   navigation[i].className = "alt-Nav-link";
-  // }
-  navigation[1].className = "alt-Nav-link";
+  console.log(navigation.length);
+  var i = 0;
+  while (i < navigation.length) {
+    navigation[i].className = "alt-Nav-link";
+    i++;
+  }
+  // navigation[3].className = "alt-Nav-link";
 }
 
 function superScriptFiveMillion() {
