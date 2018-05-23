0e4e8e7d65613afa8dad385b91c6d755780ed52c
Rick Sear <yobob.sear@gmail.com>
2018-05-23 10:57:28 -0500

    Fixed guess button

--- NumberGuesser.js
+++ NumberGuesser.js
@@ -6,17 +6,20 @@ function startGame() {
   var startSwitch = document.getElementById("starter");
   startSwitch.parentNode.removeChild(startSwitch);
   document.getElementById("output").innerHTML = "Whaddup nerd, and welcome back to your favorite number guessing game!";
+
   var gameInput = document.getElementById("theGame");
   gameInput.innerHTML = "I'm thinking of a number between 1 and 10. <br> Take a guess:";
+
   var inputBox = document.createElement("input");
   inputBox.type = "text";
   inputBox.name = "guess";
   inputBox.target = "weezinator";
   gameInput.appendChild(inputBox);
+
   var submitButton = document.createElement("button");
   submitButton.onclick = "guesserGame()";
   submitButton.innerHTML = "Guess";
-  gameInput.appendChild(inputBox);
+  gameInput.appendChild(submitButton);
 }
 
 function guesserGame() {
