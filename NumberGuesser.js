0cd3a1f9df139d3a2fd2395805b385be7e609efc
Rick Sear <yobob.sear@gmail.com>
2018-05-23 10:37:53 -0500

    Modifying effects of start button

--- NumberGuesser.js
+++ NumberGuesser.js
@@ -1,10 +1,17 @@
 function happyBirthday() {
-  window.alert("HAPPY BIRTHDAY ELLIE!")
+  //window.alert("HAPPY BIRTHDAY ELLIE!")
 }
 
 function startGame() {
   var startSwitch = document.getElementById("starter");
   startSwitch.parentNode.removeChild(startSwitch);
+  document.getElementById("output").innerHTML = "Whaddup nerd, and welcome back to your favorite number guessing game!";
+  var gameInput = document.getElementById("theGame");
+  gameInput.innerHTML = "I'm thinking of a number between 1 and 10. <br> Take a guess:";
+  var inputBox = document.createElement(input);
+  inputBox.setAttribute('type', text);
+  inputBox.setAttribute('name', guess);
+  gameInput.appendChild(inputBox);
 }
 
 function guesserGame() {}
