cbb9e40339cb3058824de1187a0986b31fbc6bd6
Rick Sear <yobob.sear@gmail.com>
2018-05-23 10:22:52 -0500

    Start button

--- NumberGuesser.js
+++ NumberGuesser.js
@@ -2,4 +2,9 @@ function happyBirthday() {
   window.alert("HAPPY BIRTHDAY ELLIE!")
 }
 
+function startGame() {
+  var startSwitch = document.getElementById("starter");
+  startSwitch.parentNode.removeChild(startSwitch);
+}
+
 function guesserGame() {}
