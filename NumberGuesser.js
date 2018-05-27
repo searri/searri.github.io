7188585711a10dc9e6faf7dfbc5a0a46a433d35e
Rick Sear <yobob.sear@gmail.com>
2018-05-27 16:19:18 -0400

    Check for compatibility with local storage

--- NumberGuesser.js
+++ NumberGuesser.js
@@ -3,6 +3,14 @@ function happyBirthday() {
   console.log("weezy");
 }
 
+function checkLocalStorage() {
+  if (typeof(Storage) !== "undefined") {
+    startGame2();
+  } else {
+    window.alert("Sorry, this version is incompatible with your browser.")
+  }
+}
+
 function startGame1() {
   var startSwitch = document.getElementById("starter1"); //add text to welcome field
   startSwitch.parentNode.removeChild(startSwitch);
@@ -32,8 +40,37 @@ function startGame1() {
   };
   submitButton.innerHTML = "Try your luck";
   gameInput.appendChild(submitButton);
+}
 
-  // numGen();
+function startGame2() {
+  var startSwitch = document.getElementById("starter2"); //add text to welcome field
+  startSwitch.parentNode.removeChild(startSwitch);
+  document.getElementById("welcome").innerHTML = "Whaddup nerd, and welcome back to your favorite number guessing game!";
+
+  document.getElementById("starter1").parentNode.removeChild(document.getElementById("starter1")); //remove other version buttons
+
+  var gameInput = document.getElementById("prompt"); //add text to prompt field
+  gameInput.innerHTML = "<br>I'm thinking of a number between 1 and 10. <br> Take a guess:";
+
+  var inputBox = document.createElement("input"); //Create the input box
+  inputBox.type = "text";
+  inputBox.id = "inputBox";
+  gameInput.appendChild(inputBox);
+
+  var submitButton = document.createElement("button"); //create the guess button
+  submitButton.onclick = function() { //Function associated with the guess button
+    var guess = document.getElementById("inputBox").value;
+    var r = numGen(10);
+    console.log(r);
+    console.log(guess);
+    if (r == guess) {
+      document.getElementById("welcome").innerHTML = "Wow. Good job. You guessed my number. Well freakin done."
+    } else {
+      document.getElementById("welcome").innerHTML = "WRONG-O! To be fair, though, the odds aren't on your side."
+    }
+  };
+  submitButton.innerHTML = "Try your luck";
+  gameInput.appendChild(submitButton);
 }
 
 function numGen(s) {
