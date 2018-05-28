e29379093e72efc5c854fafbad4b994f575164d9
Rick Sear <yobob.sear@gmail.com>
2018-05-28 15:07:46 -0400

    Bit of debugging

--- NumberGuesser.js
+++ NumberGuesser.js
@@ -26,9 +26,9 @@ function startGame1() {
     console.log(r);
     console.log(guess);
     if (r == guess) {
-      document.getElementById("welcome").innerHTML = "Wow. Good job. You guessed my number. Well freakin done."
+      document.getElementById("welcome").innerHTML = "Wow. Good job. You guessed my number. Well freakin done.";
     } else {
-      document.getElementById("welcome").innerHTML = "WRONG-O! To be fair, though, the odds weren't on your side."
+      document.getElementById("welcome").innerHTML = "WRONG-O! To be fair, though, the odds weren't on your side.";
     }
   };
   submitButton.innerHTML = "Spam it! Eventually it'll work.";
@@ -60,9 +60,9 @@ function startGame2() {
     console.log(r);
     console.log(guess);
     if (r == guess) {
-      document.getElementById("welcome").innerHTML = "Wow. You guessed my number. But...you had infinite guesses, sooo..."
+      document.getElementById("welcome").innerHTML = "Wow. You guessed my number. But...you had infinite guesses, sooo...";
     } else {
-      document.getElementById("welcome").innerHTML = "Sorry, mate! Try again."
+      document.getElementById("welcome").innerHTML = "Sorry, mate! Try again.";
     }
   };
   submitButton.innerHTML = "Try your luck";
@@ -129,50 +129,51 @@ function startGame3() {
 
     ver3updatePrompt();
   }
+}
 
-  function ver3updatePrompt() {
-    var seed = localStorage.getItem("randSeed");
-    var lyfe = localStorage.getItem("life");
-    var gameInput = document.getElementById("prompt"); //add text to prompt field
-    gameInput.innerHTML = "<br>I'm thinking of a number between 1 and " + seed + ". <br> Take a guess:";
-    if (lyfe == 1) {
-      document.getElementById("lifeLabel").innerHTML = "You have " + lyfe + " life left! Don't mess up!";
-    } else {
-      document.getElementById("lifeLabel").innerHTML = "You have " + lyfe + " lives remaining.";
-    }
+function ver3updatePrompt() {
+  var seed = localStorage.getItem("randSeed");
+  var lyfe = localStorage.getItem("life");
+  var gameInput = document.getElementById("prompt"); //add text to prompt field
+  gameInput.innerHTML = "<br>I'm thinking of a number between 1 and " + seed + ". <br> Take a guess:";
+  if (lyfe == 1) {
+    document.getElementById("lifeLabel").innerHTML = "You have " + lyfe + " life left! Don't mess up!";
+  } else {
+    document.getElementById("lifeLabel").innerHTML = "You have " + lyfe + " lives remaining.";
   }
+}
 
-  function advanceLevel(l) {
-    if (l == 2) {
-      document.getElementById("level").innerHTML = "Level II";
-      localStorage.setItem("randSeed", 10);
-    } else if (l == 3) {
-      document.getElementById("level").innerHTML = "Level III";
-      localStorage.setItem("randSeed", 15);
-    } else if (l == 4) {
-      document.getElementById("level").innerHTML = "Level IV";
-      localStorage.setItem("randSeed", 20);
-    } else {
-      window.alert("Error encountered.")
-      pageRefresh();
-    }
+function advanceLevel(l) {
+  if (l == 2) {
+    document.getElementById("level").innerHTML = "Level II";
+    localStorage.setItem("randSeed", 10);
+  } else if (l == 3) {
+    document.getElementById("level").innerHTML = "Level III";
+    localStorage.setItem("randSeed", 15);
+  } else if (l == 4) {
+    document.getElementById("level").innerHTML = "Level IV";
+    localStorage.setItem("randSeed", 20);
+  } else {
+    window.alert("Error encountered.");
+    pageRefresh();
   }
+}
 
-  function numGen(s) {
-    var n = Math.floor(Math.random() * s);
-    n++;
-    console.log(n);
-    return n;
-  }
+function numGen(s) {
+  var n = Math.floor(Math.random() * s);
+  n++;
+  console.log(n);
+  return n;
+}
 
-  function checkLocalStorage() {
-    if (typeof(Storage) !== "undefined") {
-      window.alert("Your browser is compatible...\nThere must be a different issue.");
-    } else {
-      window.alert("Sorry, this version is incompatible with your browser.")
-    }
+function checkLocalStorage() {
+  if (typeof(Storage) !== "undefined") {
+    window.alert("Your browser is compatible...\nThere must be a different issue.");
+  } else {
+    window.alert("Sorry, this version is incompatible with your browser.")
   }
+}
 
-  function pageRefresh() {
-    location.reload();
-  }
+function pageRefresh() {
+  location.reload();
+}
